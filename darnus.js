1e34bc69ed976fd187cd5a793b1ca879a5dbd0b4
yasmin beer <55171112+beersprite@users.noreply.github.com>
2024-06-09 17:59:25 -0300

    next button functional

--- darnus.js
+++ darnus.js
@@ -96,51 +96,51 @@ var prompts = [
         correct_answer: 'back'
     }
 ];
-    // {
-    //     prompt: 'O hallowed moon, take fire and scorch my foes!',
-    //     a: ['lunar_dynamo', 'thermiotic_beam']
-    // },
-    // {
-    //     prompt: 'O hallowed moon, shine you the iron path!',
-    //     a: ['lunar_dynamo', 'iron_chariot']
-    // },
-    // {
-    //     prompt: 'Blazing path, lead me to iron rule!',
-    //     a: ['thermiotic_beam', 'iron_chariot']
-    // },
-    // {
-    //     prompt: 'Take fire, O hallowed moon!',
-    //     a: ['thermiotic_beam', 'lunar_dynamo']
-    // },
-    // {
-    //     prompt: 'From on high I descend, the iron path to call!',
-    //     a: ['ravens_dive', 'iron_chariot']
-    // },
-    // {
-    //     prompt: 'From on high I descend, the iron path to walk!',
-    //     a: ['ravens_dive', 'iron_chariot']
-    // },
-    // {
-    //     prompt: 'From on high I descend, the hallowed moon to call!',
-    //     a: ['ravens_dive', 'lunar_dynamo']
-    // },
-    // {
-    //     prompt: "Fleeting light! 'Neath the red moon, scorch you the earth!",
-    //     a: ['dalamud_dive', 'thermiotic_beam']
-    // },
-    // {
-    //     prompt: 'Fleeting light! Amid a rain of stars, exalt you the red moon!',
-    //     a: ['meteor_stream', 'dalamud_dive']
-    // },
+// {
+//     prompt: 'O hallowed moon, take fire and scorch my foes!',
+//     a: ['lunar_dynamo', 'thermiotic_beam']
+// },
+// {
+//     prompt: 'O hallowed moon, shine you the iron path!',
+//     a: ['lunar_dynamo', 'iron_chariot']
+// },
+// {
+//     prompt: 'Blazing path, lead me to iron rule!',
+//     a: ['thermiotic_beam', 'iron_chariot']
+// },
+// {
+//     prompt: 'Take fire, O hallowed moon!',
+//     a: ['thermiotic_beam', 'lunar_dynamo']
+// },
+// {
+//     prompt: 'From on high I descend, the iron path to call!',
+//     a: ['ravens_dive', 'iron_chariot']
+// },
+// {
+//     prompt: 'From on high I descend, the iron path to walk!',
+//     a: ['ravens_dive', 'iron_chariot']
+// },
+// {
+//     prompt: 'From on high I descend, the hallowed moon to call!',
+//     a: ['ravens_dive', 'lunar_dynamo']
+// },
+// {
+//     prompt: "Fleeting light! 'Neath the red moon, scorch you the earth!",
+//     a: ['dalamud_dive', 'thermiotic_beam']
+// },
+// {
+//     prompt: 'Fleeting light! Amid a rain of stars, exalt you the red moon!',
+//     a: ['meteor_stream', 'dalamud_dive']
+// },
 // ]
 
 //var ANSWERS = ['lunar_dynamo', 'thermiotic_beam', 'iron_chariot', 'ravens_dive', 'dalamud_dive', 'meteor_stream'];
-var ANSWERS = ['left', 'right', 'front', 'back'];
-var answer = [];
-var inputAnswer = [];
-var numCorrect = 0; // yeah yeah this naming is confusing
+// var ANSWERS = ['left', 'right', 'front', 'back'];
+// var answer = [];
+// var inputAnswer = [];
+var streakCounter = 0;
 var correct = false;
-var lastPrompt = 0;
+// var lastPrompt = 0;
 var timeLimit = 0;
 var timeoutEnabled = false;
 var timeoutHandle;
@@ -151,7 +151,7 @@ function getRandomPrompt() {
     return prompts[randomIndex];
 }
 
-function renderHoleDirectionImage(prompt) {
+function renderHoleDirectionImage(prompt, div) {
     let imageSrc = '';
     switch (prompt.hole_direction) {
         case 'front':
@@ -172,10 +172,11 @@ function renderHoleDirectionImage(prompt) {
 
     const imageElement = document.createElement('img');
     imageElement.src = imageSrc;
-    document.body.appendChild(imageElement);
+    imageElement.classList.add('m-3');
+    div.appendChild(imageElement);
 }
 
-function renderStepsImage(prompt) { // not used at the moment, but may b
+function renderStepsImage(prompt, div) { // not used at the moment, but may b
     let imageSrc = '';
     switch (prompt.steps) {
         case 'III':
@@ -190,10 +191,11 @@ function renderStepsImage(prompt) { // not used at the moment, but may b
 
     const imageElement = document.createElement('img');
     imageElement.src = imageSrc;
-    document.body.appendChild(imageElement);
+    imageElement.classList.add('m-3');
+    div.appendChild(imageElement);
 }
 
-function renderRotationImage(prompt) {
+function renderRotationImage(prompt, div) {
     let imageSrc = '';
     switch (prompt.rotation) {
         case 'CW':
@@ -208,75 +210,84 @@ function renderRotationImage(prompt) {
 
     const imageElement = document.createElement('img');
     imageElement.src = imageSrc;
-    document.body.appendChild(imageElement);
+    imageElement.classList.add('m-3');
+    div.appendChild(imageElement);
 }
 
 function renderImages(prompt) {
-    renderHoleDirectionImage(prompt);
-    renderStepsImage(prompt);
-    renderRotationImage(prompt);
+    const imagesContainer = document.getElementById('images-container');
+    imagesContainer.innerHTML = '';
+    renderHoleDirectionImage(prompt, imagesContainer);
+    renderStepsImage(prompt, imagesContainer);
+    renderRotationImage(prompt, imagesContainer);
 
 }
 
-const randomPrompt = getRandomPrompt();
-console.log(randomPrompt);
+let randomPrompt = getRandomPrompt();
 renderImages(randomPrompt);
 //
 // let prompt = document.getElementById('prompt');
 // let n = Math.floor(Math.random() * prompts.length);
 // prompt.innerText = prompts[n].prompt;
 // answer = prompts[n].a;
-
 var nextButton = document.getElementById('next');
+
+function resetButtonStyles() {
+const answerButtons = document.querySelectorAll('.answer-button');
+    answerButtons.forEach((a) => {
+        a.className = 'btn btn-primary';
+    });
+}
 nextButton.onclick = () => {
     correct = false;
+    resetButtonStyles();
     startTimerIfEnabled();
-
-    let prompt = document.getElementById('prompt');
-
-    prompt.className = 'd-inline';
-    ANSWERS.forEach((a) => {
-        console.log(a);
-        let answerButton = document.getElementById(a);
-        answerButton.className = 'btn btn-primary';
-    });
-
-    let n = 1 + Math.floor(Math.random() * (prompts.length-1));
-    n = (lastPrompt + n) % prompts.length;
-    lastPrompt = n;
-    prompt.innerText = prompts[n].prompt;
-    answer = prompts[n].a;
-    inputAnswer = [];
+    randomPrompt = getRandomPrompt();
+    renderImages(randomPrompt);
+    console.log(randomPrompt);
 }
+//
+//
+// var nextButton = document.getElementById('next');
+// nextButton.onclick = () => {
+//     correct = false;
+//     startTimerIfEnabled();
+//
+//     let prompt = document.getElementById('prompt');
+//
+//     prompt.className = 'd-inline';
+//     ANSWERS.forEach((a) => {
+//         console.log(a);
+//         let answerButton = document.getElementById(a);
+//         answerButton.className = 'btn btn-primary';
+//     });
+//
+//     let n = 1 + Math.floor(Math.random() * (prompts.length-1));
+//     n = (lastPrompt + n) % prompts.length;
+//     lastPrompt = n;
+//     prompt.innerText = prompts[n].prompt;
+//     answer = prompts[n].a;
+//     inputAnswer = [];
+// }
 
 var answerButtons = document.getElementById('answerButtons');
 answerButtons.onclick = (e) => {
-    if(e.target.id === 'answerButtons') {return;}
+    if (e.target.id === 'answerButtons') return;
     console.log(e.target.id);
-    let a = e.target.id;
 
-    if(a === answer[inputAnswer.length]) {
-        document.getElementById(a).className = 'btn btn-success';
-        inputAnswer.push(a);
+    let userAnswer = e.target.id;
 
-        ANSWERS.forEach((a) => {
-            let answerButton = document.getElementById(a);
-            a.className = 'btn btn-primary';
-        });
-
-        if(inputAnswer.length >= 2) {
-            let prompt = document.getElementById('prompt');
-            prompt.className = 'd-inline bg-success';
-
-            numCorrect+=1
-            correct = true;
-        }
-    } else {
+    if (userAnswer === prompt.answer) {
+        document.getElementById(userAnswer).className = 'btn btn-success';
+        streakCounter++;
+        correct = true;
+    }
+    else {
         e.target.className = 'btn btn-danger';
-        numCorrect = 0;
+        streakCounter = 0;
     }
     let streak = document.getElementById('streak');
-    streak.innerText = `Streak: ${numCorrect}`;
+    streak.innerText = `Streak: ${streakCounter}`;
 }
 
 function timerEnableToggled(checkbox)
@@ -311,9 +322,9 @@ function resetTimer() {
 
 function failure() {
     if (correct === false) {
-        numCorrect = 0;
+        streakCounter = 0;
         let streak = document.getElementById('streak');
-        streak.innerText = `Streak: ${numCorrect}`;
+        streak.innerText = `Streak: ${streakCounter}`;
 
         alert("Time out - You died");
     }
